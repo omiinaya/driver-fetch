4a1d301107fabd53972d78bede7c385691244766
Omar Minaya <omiinaya@gmail.com>
2021-06-24 12:08:59 -0400

    handling checkboxes.

--- renderer.js
+++ renderer.js
@@ -1,9 +1,18 @@
 const electron = require('electron')
 const ipc = electron.ipcRenderer
 
+var motherboard;
+var manufacturers;
+var processors;
+
 //on DOM load
 document.addEventListener("DOMContentLoaded", function (event) {
-  //setHTML()
+  motherboard = document.getElementById('motherboard')
+  manufacturers = document.getElementById('manufacturers')
+  processors = document.getElementById('processors')
+
+
+  handleCheckbox()
   ipc.send('DEFAULT_REQUEST')
 });
 
@@ -11,13 +20,28 @@ function start() {
   ipc.send("TESTING_1")
 }
 
+function handleCheckbox() {
+  var checkbox = document.querySelector("input[name=checkbox]");
+  checkbox.addEventListener('change', function () {
+    if (this.checked) {
+      motherboard.disabled = false;
+      manufacturers.disabled = false;
+      processors.disabled = false;
+    } else {
+      motherboard.disabled = true;
+      manufacturers.disabled = true;
+      processors.disabled = true;
+    }
+  });
+}
+
 function setHTML(a, b, c) {
-  var motherboard   = document.getElementById('motherboard').value = a
-  var manufacturers = document.getElementById('manufacturers').value = b
-  var processors    = document.getElementById('processors').value = c
-  console.log(motherboard)
-  console.log(manufacturers)
-  console.log(processors)
+  var mb = motherboard.value = a
+  var mf = manufacturers.value = b
+  var pr = processors.value = c
+  console.log(mb)
+  console.log(mf)
+  console.log(pr)
 }
 
 ipc.on('LOG_REQUEST', (evt, data) => {
