585ec9a2ab7a68a7cab5bc964f9529ca57f5d58b
omiinaya <omiinaya@gmail.com>
2021-06-26 23:34:02 -0400

    unzipping download when done.

--- update.js
+++ update.js
@@ -1,8 +1,10 @@
 const puppeteer = require('puppeteer-extra')
 const progress = require('request-progress')
 const { execSync } = require('child_process')
-const { createWriteStream } = require("fs")
+const { createWriteStream, createReadStream } = require("fs")
 const request = require('request')
+const path = require('path')
+const unzipper = require('unzipper')
 
 const StealthPlugin = require('puppeteer-extra-plugin-stealth');
 puppeteer.use(StealthPlugin());
@@ -62,7 +64,7 @@ async function scrapeReleases(page) {
     );
     console.log(hrefs);
 
-    var directory = __dirname
+    var directory = path.join(__dirname, '../')
     hrefs.forEach(url => {
         dl(url, getFilePath(url, directory))
     })
@@ -84,8 +86,8 @@ async function ifNotExistCreateDir(url, directory) {
 }
 
 function getFilePath(url, directory) {
-    var name = url.substring(url.lastIndexOf('/') + 1, url.length)
-    var path = directory + '\\test\\' + name
+    var name = url.substring(url.lastIndexOf('/') + 1, url.length).replace('.zip', '')
+    var path = directory + name + '\\' + name + '.zip'
     return path
 }
 
@@ -97,8 +99,16 @@ function dl(url, directory) {
         .on('error', err => console.log(err))
         .on('end', () => {
             browser.close()
+            unzip(directory, url)
         })
         .pipe(createWriteStream(directory))
+}
+
+function unzip(directory, url) {
+    var name = url.substring(url.lastIndexOf('/') + 1, url.length)
+    var path = directory.replace(name, '')
+    createReadStream(directory)
+    .pipe(unzipper.Extract({ path: path }));
 }
 
 module.exports = {
